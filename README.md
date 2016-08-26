d817678d174c0f0a4c9ff29c28fd696bdf2eb8ad
Erich Duda <eduda@redhat.com>
2016-08-26 14:34:13 +0200

    Implemented ability to execute user-defined script or executable file.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # java-wrapper
-Wrapper for java binary which monitors standard output and if the process does not produce any logs, it sends kill -3 signal to print thread dump.
+Wrapper for java binary which monitors standard output and if the process does not produce any logs in defined timeout, it executes a user-defined script or executable file to investigate what happened with the Java process.
 
 ## Environment variables
 * WRAPPER\_JAVA\_HOME - path pointing to installation directory of JDK. Java binary located in $WRAPPER\_JAVA\_HOME/bin/java is used. If it is not specified, the /usr/bin/java file is used.
-* WRAPPER\_TIMEOUT - timeout in seconds. If the Java program does not produce any output for $WRAPPER\_TIMEOUT, the java-wrapper call _kill -3_ and after 5 seconds _kill -9_ on that process. Default value is 60.
+* WRAPPER\_TIMEOUT - timeout in seconds. If the Java program does not produce any output in $WRAPPER\_TIMEOUT, the java-wrapper calls **hang handler** defined by environments and after that *kill -9* is called. Default value is 60.
+* WRAPPER_HANG_SCRIPT - **hang handler** defined by bash script. It is called if the java process is hanging. The value is used as */bin/bash -c [value] JAVA_PID*. Default value is *kill -3 $0; sleep 5*. Give attention to *sleep 5*. The *kill -9* is called immediately after the **hang handler** finishes. *sleep 5* gives the Java process time to print the thread dump to standard output.
+* WRAPPER_HANG_EXEC - **hang handler** defined by path to the executable. If the variable is defined, it has precedence before WRAPPER_HANG_SCRIPT. By default the variable is undefined.
 
 ## Build
 ```
--- main.c
+++ main.c
@@ -5,10 +5,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
+
+pthread_mutex_t lock;
+pthread_cond_t cond;
+int hanging = 0;
+int childExit = 0;
 
 void parent(int pipe, int child);
 
 void child(int pipe, char** argv, char** environ);
+
+void* readChildOutput(void *arg);
+
+void handleHanging(int hangingPid);
 
 int main(int argc, char** argv, char** environ) {
   int p[2];
@@ -28,12 +38,77 @@ int main(int argc, char** argv, char** environ) {
     child(p[1], argv, environ);
   } else {
     perror("[JAVA_WRAPPER] Error during the fork creation.");
-    exit(2);
+    exit(1);
   }
 }
 
 void parent(int pipe, int child) {
+  pthread_t tid;
+  int err;
 
+  int args[] = {pipe, child};
+  err = pthread_create(&tid, NULL, &readChildOutput, args);
+  if (err) {
+    perror("[JAVA_WRAPPER] cannot create thread");
+    exit(1);
+  }
+
+  int handled = 0;
+
+  pthread_mutex_lock(&lock);
+
+  while (!childExit) {
+    pthread_cond_wait(&cond, &lock);
+    if (!handled && hanging) {
+      handled = 1;
+      printf("[JAVA_WRAPPER] Process is hanging\n");
+
+      int pid = fork();
+      if (pid > 0) {
+        waitpid(pid, NULL, 0);
+        printf("[JAVA_WRAPPER] calling kill -9\n");
+        kill(child, 9);
+      } else {
+        handleHanging(child);
+      }
+    }
+  }
+
+  pthread_mutex_unlock(&lock);
+
+  pthread_join(tid, NULL);
+
+}
+
+void child(int pipe, char** argv, char** environ) {
+  const char javaBin[] = "/bin/java";
+  const char defaultJava[] = "/usr/bin/java";
+  char* javaCmd;
+
+  char* javaHome = getenv("WRAPPER_JAVA_HOME");
+  if (javaHome) {
+    size_t len = strlen(javaHome) + sizeof(javaBin);
+    javaCmd = (char*) malloc(len * sizeof(char));
+    strcpy(javaCmd, javaHome);
+    strcat(javaCmd, javaBin);
+  } else {
+    javaCmd = (char*) malloc(sizeof(defaultJava) * sizeof(char));
+    strcpy(javaCmd, defaultJava);
+  }
+
+  printf("[JAVA_WRAPPER] Binary: %s\n", javaCmd);
+  fflush(stdout);
+
+  dup2(pipe, 1);
+  dup2(pipe, 2);
+
+  execve(javaCmd, argv, environ);
+  free(javaCmd);
+}
+
+void* readChildOutput(void *arg) {
+  int pipe = ((int*) arg)[0];
+  int child = ((int*) arg)[1];
   int timeout = 60;
 
   char* timeoutEnv = getenv("WRAPPER_TIMEOUT");
@@ -41,7 +116,7 @@ void parent(int pipe, int child) {
     timeout = atoi(timeoutEnv);
     if (!timeout) {
       perror("[JAVA_WRAPPER] Error parsing env WRAPPER_TIMEOUT");
-      exit(5);
+      exit(2);
     }
   }
 
@@ -52,8 +127,6 @@ void parent(int pipe, int child) {
   char buffer[bufferSize];
   fd_set rfds;
   struct timeval rtimeout;
-
-  int hanging = 0;
 
   read:
   FD_ZERO(&rfds);
@@ -72,7 +145,7 @@ void parent(int pipe, int child) {
       write(1, buffer, size);
     } else if (size < 0) {
       perror("[JAVA_WRAPPER] Unable to read childs stdout.");
-      exit(3);
+      exit(1);
     } else {
       break;
     }
@@ -80,51 +153,65 @@ void parent(int pipe, int child) {
 
   int rc = waitpid(child, NULL, WNOHANG);
   if (rc > 0) {
-    printf("[JAVA_WRAPPER] Process finished normally.\n");
+    printf("[JAVA_WRAPPER] Process finished.\n");
     fflush(stdout);
+    pthread_mutex_lock(&lock);
+    childExit = 1;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&lock);
   } else if (rc == 0) {
-    if (hanging) {
-      printf("[JAVA_WRAPPER] Process is hanging. Calling kill -9\n");
-      fflush(stdout);
-      kill(child, 9);
-      waitpid(child, NULL, 0);
-      exit(4);
-    }
     if (rtimeout.tv_sec || rtimeout.tv_usec) {
       goto read;
     }
-    printf("[JAVA_WRAPPER] Process is hanging. Calling kill -3\n");
-    fflush(stdout);
-    kill(child, 3);
-    hanging = 1;
-    timeout = 5;
+
+    pthread_mutex_lock(&lock);
+    if (!hanging) {
+      hanging = 1;
+      pthread_cond_signal(&cond);
+    }
+    pthread_mutex_unlock(&lock);
+
+    timeout = 10;
     goto read;
   } else {
     perror("[JAVA_WRAPPER] Waitpid returned error code");
-    exit(5);
+    exit(1);
   }
 }
 
-void child(int pipe, char** argv, char** environ) {
-  char* javaCmd;
+void handleHanging(int hangingPid) {
+  const char defaultScript[] = "kill -3 $0; sleep 5";
 
-  char* javaHome = getenv("WRAPPER_JAVA_HOME");
-  if (javaHome) {
-    size_t len = strlen(javaHome) + 10;
-    javaCmd = (char*) malloc(len * sizeof(char));
-    strcpy(javaCmd, javaHome);
-    strcat(javaCmd, "/bin/java");
-  } else {
-    javaCmd = (char*) malloc(14 * sizeof(char));
-    strcpy(javaCmd, "/usr/bin/java");
+  char* exec = getenv("WRAPPER_HANG_EXEC");
+  char* script = getenv("WRAPPER_HANG_SCRIPT");
+
+  char hangingPidStr[50];
+  sprintf(hangingPidStr, "%d", hangingPid);
+
+  if (!script) {
+    script = (char*) malloc(sizeof(defaultScript) * sizeof(char));
+    strcpy(script, defaultScript);
   }
 
-  printf("[JAVA_WRAPPER] Binary: %s\n", javaCmd);
-  fflush(stdout);
-
-  dup2(pipe, 1);
-  dup2(pipe, 2);
-
-  execve(javaCmd, argv, environ);
-  free(javaCmd);
+  if (exec) {
+    printf("[JAVA_WRAPPER] Calling exec: %s\n", exec);
+    fflush(stdout);
+    if (execl(exec, exec, hangingPidStr, NULL)) {
+      const char msg[] = "[JAVA_WRAPPER] error calling WRAPPER_HANG_EXEC=";
+      char* error = (char*) malloc((sizeof(msg) + sizeof(exec)) * sizeof(char));
+      strcpy(error, msg); strcpy(error, exec);
+      perror(error);
+      exit(2);
+    }
+  } else {
+    printf("[JAVA_WRAPPER] Calling script: %s\n", script);
+    fflush(stdout);
+    if (execl("/bin/bash", "/bin/bash", "-c", script, hangingPidStr, NULL)) {
+      const char msg[] = "[JAVA_WRAPPER] error calling WRAPPER_HANG_SCRIPT=";
+      char* error = (char*) malloc((sizeof(msg) + sizeof(exec)) * sizeof(char));
+      strcpy(error, msg); strcpy(error, exec);
+      perror(error);
+      exit(2);
+    }
+  }
 }
